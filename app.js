8e1c79706d3702477e52091c11823daed85ee969
Fortune Emmanuel <fortuneemmanuel18@gmail.com>
2026-05-12 18:33:12 +0100

    updated JS

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const userCardsContainer = document.querySelector('[data-user-cards-container]');
 const userCardTemplate = document.querySelector('[data-user-template]');
+
+   // SEARCH FUNCTIONALITY
+ const searchInput = document.querySelector('[data-search]');
+
+ let users = [];
+
+    searchInput.addEventListener('input', e => {
+        const value = e.target.value.toLowerCase();
+        users.forEach(user => {
+            const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value);
+            user.element.classList.toggle('hide', !isVisible);
+        });
+    });
+
+    
 // GETTING USER INFORMATION FROM API
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
@@ -12,19 +27,6 @@ fetch('https://jsonplaceholder.typicode.com/users')
             body.textContent = user.email;
             userCardsContainer.append(card)
             return { name: user.name, email: user.email, element: card };
-        });
-    });
-
-    // SEARCH FUNCTIONALITY
- const searchInput = document.querySelector('[data-search]');
-
- let users = [];
-
-    searchInput.addEventListener('input', e => {
-        const value = e.target.value.toLowerCase();
-        users.forEach(user => {
-            const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value);
-            user.element.classList.toggle('hide', !isVisible);
         });
     });
 
