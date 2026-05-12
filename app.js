927ccc47fa2dcc1b0f4c6c7f9bb6d5aa497b5dbb
Fortune Emmanuel <fortuneemmanuel18@gmail.com>
2026-05-12 04:24:56 +0100

    added extra JS

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
+const userCardsContainer = document.querySelector('[data-user-cards-container]');
+const userCardTemplate = document.querySelector('[data-user-template]');
+
 fetch('https://jsonplaceholder.typicode.com/users');
