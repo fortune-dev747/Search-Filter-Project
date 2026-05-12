b4c9bb761a9ec61572310ec18fc984d185bdb7b8
Fortune Emmanuel <fortuneemmanuel18@gmail.com>
2026-05-12 04:29:11 +0100

    added extra JS

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const userCardsContainer = document.querySelector('[data-user-cards-container]');
 const userCardTemplate = document.querySelector('[data-user-template]');
 
-fetch('https://jsonplaceholder.typicode.com/users');
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(user => {
+            const card = userCardTemplate.content.cloneNode(true).children[0];
+            const header = card.querySelector('[data-header]');
+            const body = card.querySelector('[data-body]');
+            header.textContent = user.name;
+            body.textContent = user.email;
+            userCardsContainer.appendChild(card);
+        });
+    });
