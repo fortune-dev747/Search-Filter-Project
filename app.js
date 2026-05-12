66426365702d6cc99ae6fff1bba2f67a79e2a63e
Fortune Emmanuel <fortuneemmanuel18@gmail.com>
2026-05-12 04:55:30 +0100

    added JS

--- app.js
+++ app.js
@@ -21,4 +21,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
 
     searchInput.addEventListener('input', e => {
         const value = e.target.value.toLowerCase();
+        users.forEach(user => {
+            const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value);
+        })
     });
