f9cc26ce73e144f1006c2a7e0674b5ee3af76db8
Fortune Emmanuel <fortuneemmanuel18@gmail.com>
2026-05-13 05:45:22 +0100

    updated JS

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-// MODAL POPUP
+// MODAL POPUP 
 const popup = document.getElementById('myModal');
 
-
-function openPopup() {
+function openPopup () {
     popup.style.display = 'block';
 }
-function closePopup() {
+
+function closePopup () {
     popup.style.display = 'none';
 }
 
@@ -16,41 +16,36 @@ window.addEventListener('load', openPopup);
 // Search Functionality
 const searchInput = document.querySelector('[data-search]');
 let users = [];
-
 // Listening to what you typed 
 searchInput.addEventListener('input', e => {
     const value = e.target.value.toLowerCase();
-
     users.forEach(user => {
-        const isAvailable = user.name.value.toLowerCase(value) || user.email.value.toLowerCase(value);
-
-        user.element.classList('hide', !isAvailable);
-
+        const isValid = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value);
+        user.element.classList.toggle('hide', !isValid);
     });
 });
 
 
+
 // RESULTS OF THE CONTAINERS
 const userCardsContainer = document.querySelector('[data-user-cards-container]');
-const userCardTemplate = document.querySelector('[data-user-template]');
-
+const userCardsTemplate = document.querySelector('[data-user-template]');
 // GET THE INFO FROM THE API
 fetch ('https://jsonplaceholder.typicode.com/users')
 // convert the data into readable data 
-.then(response => response.json())
-.then (data => {
-
+ .then(response => response.json())
+ .then (data => {
     users = data.map(user => {
-        const card = userCardTemplate.content.cloneNode(true).children[0];
+        const card = userCardsTemplate.content.cloneNode(true).children[0];
         const header = card.querySelector('[data-header]');
-        const body = card.querySelector('[data-body]');
-
+        const body = card.querySelector('[ data-body]');
 
         header.textContent = user.name;
         body.textContent = user.email;
         userCardsContainer.append(card);
 
-        return { name: user.name, email:user.email, element:card}
-    })
-})
+        return { name:user.name, email:user.email, element:card}
+    });
+ });
+
 
